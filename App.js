8a77a46674284581a741236289e9a3ff6076f5c7
chiku <chikunwenyi@gmail.com>
2023-10-08 06:00:38 -0400

    oct 8

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, PanResponder, Text } from 'react-native';
+import { Svg, Path } from 'react-native-svg';
 
 export default function App() {
+  const [strokes, setStrokes] = useState([]);
+  const [pathData, setPathData] = useState('');
+  const [previousPoint, setPreviousPoint] = useState({ x: 0, y: 0 });
+
+  const handlePressIn = (event) => {
+    const { locationX, locationY } = event.nativeEvent;
+
+    setPathData(`M${locationX},${locationY}`);
+    setPreviousPoint({ x: locationX, y: locationY });
+  };
+
+  const handlePressOut = () => {
+    setStrokes((prevStrokes) => [...prevStrokes, pathData]);
+  };
+
+  const handlePressMove = (event) => {
+    const { locationX, locationY } = event.nativeEvent;
+
+    const pathSegment = `L${locationX},${locationY}`;
+    setPathData((prevPathData) => prevPathData.concat(pathSegment));
+    setPreviousPoint({ x: locationX, y: locationY });
+  };
+
+  const handleClear = () => {
+    setStrokes([]);
+    setPathData('');
+  };
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: handlePressIn,
+    onPanResponderMove: handlePressMove,
+    onPanResponderRelease: handlePressOut,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} {...panResponder.panHandlers}>
+      <Svg
+        width="100%"
+        height="100%"
+      >
+        {strokes.map((stroke, index) => (
+          <Path
+            key={index}
+            d={stroke}
+            fill="none"
+            stroke="black"
+            strokeWidth="2"
+          />
+        ))}
+        <Path
+          d={pathData}
+          fill="none"
+          stroke="black"
+          strokeWidth="2"
+        />
+      </Svg>
+      <TouchableOpacity style={styles.clearButton} onPress={handleClear}>
+        <Text style={styles.clearButtonText}>Clear</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +70,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  clearButton: {
+    position: 'absolute',
+    bottom: 20,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: 'red',
+  },
+  clearButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
